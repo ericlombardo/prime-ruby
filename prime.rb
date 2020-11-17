34adfd934c1de51d235d63d289ceafde8892098d
Eric <eric.m.lombardo@gmail.com>
2020-11-17 16:19:18 -0500

    commit tree a5d9a84e8db40da8181d9020b0fb0124b2b9fc11

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  return false if num < 2   # => false if number is not defined as prime
+  test_numbers = (2..(num - 1)).to_a    # creates an array of divisible numbers
+  # check if number is 2 || not evenly divided a.k.a prime => true if so
+  test_numbers.all? { | number | num == 2 || num % number != 0}   
+end
